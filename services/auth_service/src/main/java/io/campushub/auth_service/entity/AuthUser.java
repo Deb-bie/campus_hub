a367cdc0fd1e8779ec2a003b5b4c87c88bdca12b
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-05-12 21:06:49 -0500

    implement user details

---
+++ services/auth_service/src/main/java/io/campushub/auth_service/entity/AuthUser.java
@@ -0,0 +1,76 @@
+package io.campushub.auth_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "auth_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AuthUser implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+    private UUID auth_id;
+
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Timestamp created_at;
+
+    @UpdateTimestamp
+    @Column(updatable = true, name = "updated_at")
+    private Timestamp updated_at;
+
+    @Column(updatable = true)
+    private Timestamp last_login;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
