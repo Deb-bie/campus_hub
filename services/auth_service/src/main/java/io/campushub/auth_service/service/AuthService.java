cd1e9d784d60d241884a2b003a01339b8ee11e4d
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-05-13 22:18:55 -0500

    auth signin service

--- services/auth_service/src/main/java/io/campushub/auth_service/service/AuthService.java
+++ services/auth_service/src/main/java/io/campushub/auth_service/service/AuthService.java
@@ -1,23 +1,46 @@
 package io.campushub.auth_service.service;
 
+import io.campushub.auth_service.config.JwtService;
+import io.campushub.auth_service.dto.requests.SignInRequestDto;
 import io.campushub.auth_service.dto.requests.SignUpRequestDto;
 import io.campushub.auth_service.dto.responses.ResponseHandler;
 import io.campushub.auth_service.entity.AuthUser;
 import io.campushub.auth_service.exceptions.AlreadyExistsException;
+import io.campushub.auth_service.exceptions.NotFoundException;
 import io.campushub.auth_service.exceptions.SchoolAccountException;
 import io.campushub.auth_service.repository.AuthRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+// TODO: when I log in or sign up, i have to set the last loggined in
+// TODO: chane the type of the body message for the response handler
+
 @Service
 public class AuthService {
     private final AuthRepository authRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(AuthRepository authRepository) {
+    public AuthService(
+            AuthRepository authRepository,
+            AuthenticationManager authenticationManager,
+            JwtService jwtService,
+            PasswordEncoder passwordEncoder
+    ) {
         this.authRepository = authRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public ResponseEntity<ResponseHandler> signUp(SignUpRequestDto signUpRequestDto) throws Exception {
@@ -28,11 +51,12 @@ public class AuthService {
         }
 
         if (validateEmail(signUpRequestDto.getEmail())) {
+            String encodedPassword = passwordEncoder.encode(signUpRequestDto.getPassword());
             AuthUser authUser = AuthUser.builder()
                     .firstName(signUpRequestDto.getFirstName())
                     .lastName(signUpRequestDto.getLastName())
                     .email(signUpRequestDto.getEmail())
-                    .password(signUpRequestDto.getPassword())
+                    .password(encodedPassword)
                     .build();
             authRepository.save(authUser);
         }
@@ -50,6 +74,42 @@ public class AuthService {
                         .build()
                 );
     }
+
+    public ResponseEntity<ResponseHandler> signIn(SignInRequestDto signInRequestDto) throws Exception {
+        String jwt = "";
+        Optional<AuthUser> emailExists = authRepository.findByEmail(signInRequestDto.getEmail());
+        if (emailExists.isPresent()) {
+
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            signInRequestDto.getEmail(),
+                            signInRequestDto.getPassword()
+                    )
+            );
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            UserDetails userDetails = (UserDetails) auth.getPrincipal();
+            jwt = jwtService.generateToken(userDetails);
+
+        } else {
+            throw new NotFoundException("Email does not exists");
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                        ResponseHandler
+                                .builder()
+                                .status(HttpStatus.OK)
+                                .statusCode(HttpStatus.OK.value())
+                                .message(
+                                        jwt
+                                )
+                                .build()
+                );
+
+    }
+
+
 
     public boolean validateEmail(String email) {
         return email.endsWith(".edu");
