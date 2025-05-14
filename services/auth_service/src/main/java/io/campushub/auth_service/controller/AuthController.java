cb64743b95d3d73f82953cf4faf56e25c5ac3799
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-05-14 16:23:12 -0500

    auth signin controller

--- services/auth_service/src/main/java/io/campushub/auth_service/controller/AuthController.java
+++ services/auth_service/src/main/java/io/campushub/auth_service/controller/AuthController.java
@@ -1,5 +1,6 @@
 package io.campushub.auth_service.controller;
 
+import io.campushub.auth_service.dto.requests.SignInRequestDto;
 import io.campushub.auth_service.dto.requests.SignUpRequestDto;
 import io.campushub.auth_service.dto.responses.ResponseHandler;
 import io.campushub.auth_service.service.AuthService;
@@ -13,18 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/auth")
-public class AuthController {
-    private final AuthService authService;
+public class AuthController<T> {
+    private final AuthService<T> authService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService<T> authService) {
         this.authService = authService;
     }
 
     @Operation(summary = "sign up")
     @ApiResponse(responseCode = "201", description = "successful")
     @PostMapping("/signup")
-    public ResponseEntity<ResponseHandler> signUp(@RequestBody SignUpRequestDto signUpRequestDto) throws Exception {
+    public ResponseEntity<ResponseHandler<String>> signUp(@RequestBody SignUpRequestDto signUpRequestDto) throws Exception {
         return this.authService.signUp(signUpRequestDto);
+    }
+
+    @Operation(summary = "sign in")
+    @ApiResponse(responseCode = "200", description = "successful")
+    @PostMapping("/signin")
+    public ResponseEntity<ResponseHandler<String>> signIn(@RequestBody SignInRequestDto signInRequestDto) throws Exception {
+        return this.authService.signIn(signInRequestDto);
     }
 
 }
