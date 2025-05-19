7026c8f384d2577af1591517fef2aaac3da5f2d6
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-05-18 21:01:42 -0500

    excluding the datasource auto config

--- services/auth_service/src/test/java/io/campushub/auth_service/AuthServiceApplicationTests.java
+++ services/auth_service/src/test/java/io/campushub/auth_service/AuthServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package io.campushub.auth_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class AuthServiceApplicationTests {
 
 	@Test
