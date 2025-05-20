6f371d451233dc889e58bd1b7ad6c807b494558f
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-05-19 21:23:30 -0500

    added user profile model

--- services/user_profile_service/app/models/user.py
+++ services/user_profile_service/app/models/user.py
@@ -1,7 +1,8 @@
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, Boolean # type: ignore
+from sqlalchemy import Column, String, DateTime # type: ignore
 from sqlalchemy.ext.declarative import declarative_base # type: ignore
+from sqlalchemy.dialects.postgresql import UUID # type: ignore
 
 
 # Create a base class for declarative models
@@ -9,12 +10,27 @@ Base = declarative_base()
 
 
 class UserProfile(Base):
-    __tablename__ = "user"
+    __tablename__ = "user_profile"
 
-    id = Column(
-        uuid,
+    user_id = Column(
+        UUID(as_uuid=True),
         primary_key=True,
+        default=uuid.uuid4,
         index=True
+    )
+
+    email = Column(
+        String
+    )
+
+    first_name = Column(
+        String,
+        nullable=False
+    )
+
+    last_name = Column (
+        String,
+        nullable=False
     )
 
     bio = Column(
@@ -22,9 +38,14 @@ class UserProfile(Base):
     )
 
     created_at = Column(
-        datetime
+        DateTime,
+        default=datetime.now(),
+        nullable=False
     )
 
     updated_at = Column(
-        datetime
+        DateTime,
+        default=datetime.now(),
+        onupdate=datetime.now(),
+        nullable=False
     )
