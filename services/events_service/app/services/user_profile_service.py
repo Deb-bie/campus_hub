0cc9cc2b9cf2685e18e72d8521f9339093accc38
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-06-04 21:18:58 -0500

    user profile service

---
+++ services/events_service/app/services/user_profile_service.py
@@ -0,0 +1,32 @@
+import os
+import requests # type: ignore
+
+
+def get_user_by_email(user_email):
+    profile_url = f"{os.getenv("API_GATEWAY_URL")}/user-profile/api/v1/users/profile/{user_email}"
+    response = requests.get(profile_url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def get_user_by_id(user_id):
+    profile_url = f"{os.getenv("API_GATEWAY_URL")}/user-profile/api/v1/users/profile/{user_id}"
+    response = requests.get(profile_url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+    
+
+def get_current_user(token):
+    profile_url = f"{os.getenv("API_GATEWAY_URL")}/user-profile/api/v1/users/profile/me"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(profile_url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
