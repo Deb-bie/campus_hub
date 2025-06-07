4c776196e26bdae5aefa5ed559a5f93b2e445a9a
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-06-07 13:54:51 -0500

    changed function name

--- services/events_service/app/services/user_profile_service.py
+++ services/events_service/app/services/user_profile_service.py
@@ -2,8 +2,8 @@ import os
 import requests # type: ignore
 
 
-def get_user_by_email(user_email):
-    profile_url = f"{os.getenv("API_GATEWAY_URL")}/user-profile/api/v1/users/profile/{user_email}"
+def get_current_user_by_email(user_email):
+    profile_url = f"{os.getenv("API_GATEWAY_URL")}/api/v1/user-profile/users/profile/{user_email}"
     response = requests.get(profile_url)
 
     if response.status_code == 200:
