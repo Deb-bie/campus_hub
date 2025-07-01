c91f17afdf1c9abde8838d3cd7a9042d8ce6f4af
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-06-30 21:37:22 -0500

    exceptions

---
+++ services/events_service/app/utils/exceptions.py
@@ -0,0 +1,7 @@
+class SearchServiceError(Exception):
+    """Exception raised for errors in the search service"""
+    pass
+
+class ValidationError(Exception):
+    """Exception raised for validation errors"""
+    pass
