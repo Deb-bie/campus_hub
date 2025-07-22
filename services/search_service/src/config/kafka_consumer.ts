4ddb8f1b357858b18da62a0da928c782c5c083a9
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-22 01:09:02 -0500

    kafka consumer for event created

---
+++ services/search_service/src/config/kafka_consumer.ts
@@ -0,0 +1,56 @@
+import { Kafka } from 'kafkajs';
+import { loadEnv } from './config';
+import eventController from '../controllers/eventController';
+
+
+loadEnv()
+
+const kafka = new Kafka(
+    { 
+        clientId: process.env.CLIENT_ID, 
+        brokers: [process.env.KAFKA_BROKER!]
+    }
+);
+
+const consumer = kafka.consumer(
+    {
+        groupId: process.env.GROUP_ID!
+    }
+);
+
+
+export const startConsumer = async () => {
+    await consumer.connect();
+
+    await consumer.subscribe(
+        { 
+            topic: 'event_created', 
+            fromBeginning: true 
+        }
+    );
+
+    await consumer.subscribe(
+        { 
+            topic: 'event_updated_topic', 
+            fromBeginning: true 
+        }
+    );
+    
+    await consumer.subscribe(
+        { 
+            topic: 'event_deleted_topic', 
+            fromBeginning: true 
+        }
+    );
+
+    await consumer.run({
+        eachMessage: async ({ topic, message }) => {
+            const value = message.value?.toString();
+            const payload = JSON.parse(value!);
+
+            if (topic === 'event_created') {
+                await eventController.indexEventToElastic(payload.metadata)
+            } 
+        }
+    });
+};
