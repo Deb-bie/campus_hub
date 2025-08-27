ce59c1a20dd3be31796ec8623baf8da7eb90a276
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-08-26 19:06:04 -0500

    auth response and profileservice dto

--- services/auth_service/src/main/java/io/campushub/auth_service/dto/responses/AuthResponseDto.java
+++ services/auth_service/src/main/java/io/campushub/auth_service/dto/responses/AuthResponseDto.java
@@ -1,9 +1,13 @@
 package io.campushub.auth_service.dto.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class SignInResponseDto {
     private String userId;
