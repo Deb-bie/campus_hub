f96e0d1e714d623c427762b51660ee96e40c7a6c
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-08-28 20:41:03 -0500

    renamed category enum

--- services/events_service/app/enums/category_enum.py
+++ services/events_service/app/enums/category_enum.py
@@ -2,13 +2,13 @@ import enum
 from sqlalchemy import Enum # type: ignore
 
 class CategoryEnum(enum.Enum):
-    CULTURAL_DIVERSITY = "cultural & diversity"
+    CULTURAL = "cultural"
     ACADEMIC = "academic"
-    SOCIAL_ENTERTAINMENT = "social & entertainment"
-    SPORTS_FITNESS = "sports & fitness"
-    CAREER_PROFESSIONAL_DEVELOPMENT = "career & professional development"
-    HEALTH_WELLNESS = "health & wellness"
-    CLUBS_ORGANIZATIONS = "clubs & organizations"
-    RESIDENTIAL_LIFE = "residential life"
-    MONEY_FINANCE = "money & finance"
+    SOCIAL = "social"
+    SPORTS = "sports"
+    CAREER = "career"
+    HEALTH = "health"
+    WORKSHOP = "workshop"
+    FINANCE = "finance"
+    OTHER = "other"
 
